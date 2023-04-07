8669e01eccf8408c81f988b921ca2eeb34d779db
liesenf <chenliesenfeld@gmail.com>
2023-04-07 17:08:25 +0200

    added first class

---
+++ sktalk/csv_to_json.py
@@ -0,0 +1,13 @@
+"""Documentation about the scikit-talk module - csv to json."""
+import pandas as pd
+
+class Corpus:
+    def __init__(self,path):
+        self.path = path
+        
+    def return_dataframe(self):
+        self.df = pd.read_csv(self.path)
+
+    def return_json(self):
+        self.return_dataframe()
+        self.json = self.df.to_json()
