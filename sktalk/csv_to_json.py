0c31614ef09f3eab371f553be638010eab5b9ff5
Barbara Vreede <b.vreede@esciencecenter.nl>
2023-04-07 19:36:45 +0200

    fix linter issues

--- sktalk/csv_to_json.py
+++ sktalk/csv_to_json.py
@@ -1,10 +1,11 @@
 """Documentation about the scikit-talk module - csv to json."""
 import pandas as pd
 
+
 class Corpus:
-    def __init__(self,path):
+    def __init__(self, path):
         self.path = path
-        
+
     def return_dataframe(self):
         self.df = pd.read_csv(self.path)
 
