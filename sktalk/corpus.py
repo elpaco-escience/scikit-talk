d1f8e82c6c4cce14f3f537ce41745d08763aa683
Barbara Vreede <b.vreede@esciencecenter.nl>
2023-05-19 17:14:07 +0200

    add save to jsonl method

--- sktalk/corpus.py
+++ sktalk/corpus.py
@@ -1,3 +1,5 @@
+import json
+import os
 import pandas as pd
 
 
@@ -31,3 +33,19 @@ class Corpus:
         """
         speakers = self.df[speakercol]
         self.speakers = list(set(speakers))
+
+    def save_jsonl(self, name, directory):
+        """Save the dataframe as a jsonl file.
+
+        Args:
+            name (str): The name of the corpus that will be the file name.
+            directory (str): The path to the directory where the .jsonl
+            file will be saved.
+        """
+        name = f"{name}.jsonl"
+        destination = os.path.join(directory, name)
+        with open(destination, "w") as file:
+            for _, row in self.df.iterrows():
+                json_object = row.to_dict()
+                json.dump(json_object, file)
+                file.write('\n')
