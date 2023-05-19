de42b45e4d1ec3c2479b712c07404dbf703ea30a
Barbara Vreede <b.vreede@esciencecenter.nl>
2023-05-19 15:29:25 +0200

    add documentation

--- sktalk/corpus.py
+++ sktalk/corpus.py
@@ -1,12 +1,32 @@
-"""Documentation about the scikit-talk module - corpus."""
 import pandas as pd
 
-
 class Corpus:
+    """The Corpus class represents a conversation corpus.
+
+    Args:
+        path (str): The path to the CSV file containing the corpus data.
+
+    Attributes:
+        df (pandas.DataFrame): The pandas DataFrame representing the corpus data.
+        json (str): The JSON representation of the corpus data.
+        speakers (list): The list of unique speakers extracted from the corpus data.
+    """
+
     def __init__(self, path):
+        """Initialize a new instance of the Corpus class.
+
+        Args:
+            path (str): The path to the CSV file containing the corpus data.
+        """
         self.df = pd.read_csv(path)
         self.json = self.df.to_json()
 
-    def extract_speakers(self):
-        self.speakers = self.df['participant']
+    def extract_speakers(self, speakercol='participant'):
+        """Extract the unique speakers from the corpus data.
+
+        Args:
+            speakercol (str, optional): The name of the column containing the speaker information. Default is 'participant'.
+        """
+        speakers = self.df[speakercol]
+        self.speakers = list(set(speakers))
 
