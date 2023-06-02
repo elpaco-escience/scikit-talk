a5a1cfa16854ae1a25231f735e969b40779932da
Carsten Schnober <c.schnober@esciencecenter.nl>
2023-06-02 15:59:11 +0200

    Refine utterance.py

--- sktalk/corpus/parsing/cha.py
+++ sktalk/corpus/parsing/cha.py
@@ -1,13 +1,22 @@
 from .parser import Parser
+from ..conversation import Conversation
+from ..utterance import Utterance
+
 import pylangacq
 
+
 class ChaParser(Parser):
-  def parse(self, file):
-    chatfile = pylangacq.read_chat(file)
-    chat_utterances = chatfile.utterances(by_files=False)
-    for row in chat_utterances:
-      utterance_dict = []
-      utterance_dict['speaker'] = row.participant
-      utterance_dict['time'] = str(row.time_marks)
-      utterance_dict['utterance'] = str(row.tiers)
-      utterance_dict['source'] = file
+    def parse(self, file):
+        chatfile = pylangacq.read_chat(file)
+        chat_utterances = chatfile.utterances(by_files=False)
+
+        utterances = [self._to_utterance(row, file) for row in chat_utterances]
+        return Conversation(utterances)
+
+    def _to_utterance(self, row, file):
+        return Utterance(
+            speaker=row.participant,
+            time=str(row.time_marks),
+            utterance=str(row.tiers),
+            source=file,
+        )
