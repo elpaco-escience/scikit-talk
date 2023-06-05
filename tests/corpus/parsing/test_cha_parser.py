06e29fe73379db7e629f44421dfe5e08002e018b
Carsten Schnober <c.schnober@esciencecenter.nl>
2023-06-05 11:27:27 +0200

    Add dummy TestChaparser.

---
+++ tests/corpus/parsing/test_cha_parser.py
@@ -0,0 +1,8 @@
+class TestChaParser:
+    def test_parse(self):
+        # TODO:
+        # 1. add test file to repo
+        # 2. define expected output
+        # 3. run parser on test file
+        # assert ChaParser().parse(file) == expected
+        pass
