ecc91057f13723784a8ee9678ae995ab228f87d1
Barbara Vreede <b.vreede@esciencecenter.nl>
2023-06-09 14:54:28 +0200

    update script to test function

--- notebooks/load_data.py
+++ notebooks/load_data.py
@@ -2,5 +2,5 @@ from sktalk.corpus.parsing.cha import ChaParser
 
 if __name__ == "__main__":
     french = ChaParser().parse("../data/french1/5136.cha")
-    print(french.utterances)
+    print(type(french))
 
