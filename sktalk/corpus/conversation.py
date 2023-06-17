cfe7867183a6f021733a76a3dc2799b36c3b0bf7
Barbara Vreede <b.vreede@esciencecenter.nl>
2023-06-17 14:26:37 +0200

    add documentation to conversation

--- sktalk/corpus/conversation.py
+++ sktalk/corpus/conversation.py
@@ -2,20 +2,43 @@ class Conversation:
     def __init__(
         self, utterances: list["Utterance"], metadata: dict = None  # noqa: F821
     ) -> None:
+        """Representation of a transcribed conversation
+
+        Args:
+            utterances (list[Utterance]): A list of Utterance objects representing the utterances in the conversation.
+            metadata (dict, optional): Additional metadata associated with the conversation. Defaults to None.
+        """
         self._utterances = utterances
         self._metadata = metadata
 
     @property
     def utterances(self):
+        """
+        Get the list of utterances in the conversation.
+
+        Returns:
+            list[Utterance]: A list of Utterance objects representing the utterances in the conversation.
+        """
+
         return self._utterances
 
     @property
     def metadata(self):
+        """
+        Get the metadata associated with the conversation.
+
+        Returns:
+            dict: Additional metadata associated with the conversation.
+        """
         return self._metadata
 
     def get_utterance(self, index) -> "Utterance":  # noqa: F821
         pass
 
     def summarize(self):
-        for utterance in self._utterances:
+        """
+        Print a summary of the conversation.
+        """
+        for utterance in self._utterances[:10]:
             print(utterance)
+
