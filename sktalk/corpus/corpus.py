edaadb74075beda2b96fc78cc61e01d3a3a4f5d5
Barbara Vreede <b.vreede@esciencecenter.nl>
2023-10-31 10:27:14 +0100

    add conversation to a corpus and check type

--- sktalk/corpus/corpus.py
+++ sktalk/corpus/corpus.py
@@ -1,5 +1,6 @@
 from .parsing.json import JsonFile
 from .parsing.xml import XmlFile
+from sktalk.corpus.conversation import Conversation
 
 
 class Corpus:
@@ -7,10 +8,25 @@ class Corpus:
         self, conversations: list["Conversation"] = None, **metadata: dict  # noqa: F821
     ):
         self._conversations = conversations or []
+        for conversation in self._conversations:
+            if not isinstance(conversation, Conversation):
+                raise TypeError("All conversations should be of type Conversation")
         self._metadata = metadata
 
     def __add__(self, other: "Corpus") -> "Corpus":
         pass
+
+    def append(self, conversation: Conversation):
+        """
+        Append a conversation to the Corpus
+
+        Args:
+            conversation (Conversation): Conversation object that should be added to the Corpus
+        """
+        if isinstance(conversation, Conversation):
+            self._conversations.append(conversation)
+        else:
+            raise TypeError("Conversations added should be of type Conversation")
 
     def return_json(self):
         self.return_dataframe()
