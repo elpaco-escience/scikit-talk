ccd4bc4e10b224675a5fd35880509cfab1fbbd09
Barbara Vreede <b.vreede@esciencecenter.nl>
2023-12-01 15:06:26 +0100

    Generate turn type dynamics (#1)
    * code from notebook
    
    * add turndynamics code from notebook
    
    * instructions for disabling the bloody githook
    
    * move code from test to notebook
    
    * remove notebook from repository
    
    * start adding utterance functions
    
    * add calculated fields to dataclass
    
    * add python 3.6 to show that it breaks
    
    * add python 3.8 to show that it breaks
    
    * add python 3.6 to show that it breaks
    
    * add python 3.8 to show that it breaks
    
    * remove earlier python versions again
    
    * add fields to initial data class
    
    * implement until method calculating time differences between utterances
    
    * inmplement subconversation and until next method at conversation object
    
    * autopep8
    
    * elaborate subconversation
    
    * move time processing to utterance
    
    * object oriented and small fixes
    
    * refactor post init
    
    * add subconversation functionaliry
    
    * subconversation can select based on index or time
    
    * make linter happy
    
    * also pack arguments for second subconversation test
    
    * fix linting issues
    
    * add dyadic property
    
    * apply conversation wide calculations dyadic and time to nxt
    
    * subconversation is internal
    
    * count number of participants
    
    * calculate FTO
    
    * remove old code
    
    * address linter comments
    
    * address linter issues and update fto calculation
    
    * fix noqa
    
    * fix noqa
    
    * calculations update in metadata corrected
    
    * allow warning supppression on empty conversations inside subconversation
    
    * refer to hidden _utterances instead of property
    
    * allow participant counting to exclude None
    
    * add test for FTO calculation
    
    * ensure participant count does not include future utterances
    
    * split subconversation into two functions
    
    * fix linter issue
    
    * update example notebook
    
    * Update sktalk/corpus/conversation.py
    
    Co-authored-by: Ji Qi <92043159+jiqicn@users.noreply.github.com>
    
    * Update sktalk/corpus/conversation.py
    
    Co-authored-by: Ji Qi <92043159+jiqicn@users.noreply.github.com>
    
    * add comments re: error
    
    * Update sktalk/corpus/conversation.py
    
    Co-authored-by: Ji Qi <92043159+jiqicn@users.noreply.github.com>
    
    * rewrite FTO calculation
    
    * rename overlap function to make it available
    
    * update FTO calculation to account for partial overlap
    
    * refactor overlap functions
    
    * Update sktalk/corpus/parsing/cha.py
    
    Co-authored-by: Ji Qi <92043159+jiqicn@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Ji Qi <92043159+jiqicn@users.noreply.github.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -24,20 +24,134 @@ def convo_meta():
 
 @pytest.fixture
 def convo_utts():
-    utterance1 = Utterance(
-        utterance="Hello",
-        participant="A"
-    )
-    utterance2 = Utterance(
-        utterance="Monde",
-        participant="B"
-    )
-    return [utterance1, utterance2]
+    return [
+        Utterance(
+            utterance="0 utterance A",
+            participant="A",
+            time=[0, 1000]
+        ),
+        Utterance(
+            utterance="1 utterance B",
+            participant="B",
+            time=[900, 3500]
+        ),
+        Utterance(
+            utterance="2 utterance C",
+            participant="A",
+            time=[1001, 8500]
+        ),
+        Utterance(
+            utterance="3 utterance D",
+            participant="B",
+            time=[1200, 1999]
+        ),
+        Utterance(
+            utterance="4 utterance E",
+            participant="A",
+            time=[3500, 4500]
+        ),
+        Utterance(
+            utterance="5 utterance U",
+            participant="B",
+            time=[5000, 8000]
+        ),
+        Utterance(
+            utterance="6 utterance F",
+            participant="C",
+            time=[5500, 7500]
+        ),
+        Utterance(
+            utterance="7 utterance G",
+            participant=None,
+            time=None
+        ),
+        Utterance(
+            utterance="8 utterance H",
+            participant="B",
+            time=[9000, 12500]
+        ),
+        Utterance(
+            utterance="9 utterance I",
+            participant="C",
+            time=[12000, 13000]
+        )
+    ]
 
 
 @pytest.fixture
-def my_convo(convo_utts, convo_meta):
+def convo(convo_utts, convo_meta):
     return Conversation(convo_utts, convo_meta)
+
+
+@pytest.fixture
+def utterances_for_fto():
+    return [
+        Utterance(
+            utterance="utt 0 - A",
+            participant="A",
+            time=[0, 1000]
+        ),
+        Utterance(
+            utterance="utt 1 - B",
+            participant="B",
+            time=[200, 300]
+        ),
+        Utterance(
+            utterance="utt 2 - B",
+            participant="B",
+            time=[400, 500]
+        ),
+        Utterance(
+            utterance="utt 3 - B",
+            participant="B",
+            time=[600, 900]
+        ),
+        Utterance(
+            utterance="utt 4 - B",
+            participant="B",
+            time=[1100, 1500]
+        ),
+        Utterance(
+            utterance="utt 5 - A",
+            participant="A",
+            time=None
+        ),
+        Utterance(
+            utterance="utt 6 - A",
+            participant="A",
+            time=[1300, 1800]
+        ),
+        Utterance(
+            utterance="utt 7 - B",
+            participant="B",
+            time=[1450, 1800]
+        ),
+        Utterance(
+            utterance="utt 8 - None",
+            participant=None,
+            time=[1850, 1900]
+        ),
+        Utterance(
+            utterance="utt 9 - A",
+            participant="A",
+            time=[1900, 2300]
+        ),
+        Utterance(
+            utterance="utt 10 - B",
+            participant="B",
+            time=[2200, 2400]
+        ),
+        Utterance(
+            utterance="utt 11 - B",
+            participant="B",
+            time=[2450, 2600]
+        ),
+    ]
+
+
+@pytest.fixture
+def convo_fto(utterances_for_fto, convo_meta):
+    return Conversation(utterances_for_fto, convo_meta)
 
 
 @pytest.fixture
