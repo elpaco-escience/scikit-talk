b19aa1a4d66d2a1dba65ab000907467519679929
Barbara Vreede <b.vreede@esciencecenter.nl>
2024-01-03 20:58:20 +0100

    start eaf parser with a loop

--- sktalk/corpus/parsing/eaf.py
+++ sktalk/corpus/parsing/eaf.py
@@ -1,5 +1,6 @@
 from ..utterance import Utterance
 from .parser import InputFile
+import xml.etree.ElementTree as ET
 
 
 class EafFile(InputFile):
@@ -8,4 +9,28 @@ class EafFile(InputFile):
         return {}
 
     def _extract_utterances(self):
-        return []
+        # once again, a terrible loop >.<
+        annotations = []
+
+        for tier in self.root.findall(".//TIER"):
+            participant = tier.get("TIER_ID")
+
+            for annotation in tier.findall(".//ALIGNABLE_ANNOTATION"):
+                begin = annotation.get("TIME_SLOT_REF1")
+                end = annotation.get("TIME_SLOT_REF2")
+                utterance = annotation.find("ANNOTATION_VALUE").text
+
+                annotations.append(
+                    Utterance(
+                        participant=participant,
+                        utterance=utterance,
+                        time=[begin, end]
+                    ))
+        return annotations
+
+    @property
+    def root(self):
+        if not hasattr(self, "_root"):
+            tree = ET.parse(self._path)
+            self._root = tree.getroot()
+        return self._root
