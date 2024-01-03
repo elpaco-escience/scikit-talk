c82b9374d561badfef512c68774ccf9eada2c583
Barbara Vreede <b.vreede@esciencecenter.nl>
2024-01-03 16:47:54 +0100

    Cha parsing with corrected timing (#53)
    * first try of home-spun cha parser
    
    * fix some linting issues
    
    * parse timing with less gregarious regex
    
    * Apply suggestions from code review
    
    Co-authored-by: Carsten Schnober <carschno@users.noreply.github.com>
    
    * classmethods should be staticmethods
    
    * extract and name all regexes
    
    * refactor information extraction
    
    * fix test with more complex participants and update regex
    
    * add test for utterance text
    
    * refactor loop
    
    * autopep8
    
    * Update sktalk/corpus/parsing/cha.py
    
    Co-authored-by: Carsten Schnober <carschno@users.noreply.github.com>
    
    * Update sktalk/corpus/parsing/cha.py
    
    Co-authored-by: Carsten Schnober <carschno@users.noreply.github.com>
    
    * fix up regex
    
    ---------
    
    Co-authored-by: Carsten Schnober <carschno@users.noreply.github.com>

--- sktalk/corpus/parsing/cha.py
+++ sktalk/corpus/parsing/cha.py
@@ -5,6 +5,14 @@ from .parser import InputFile
 
 
 class ChaFile(InputFile):
+    # participant may not be repeated on each line, hence the ?
+    PARTICIPANT_REGEX = r"(^\*(?P<participant>[^:]+)\:)?"
+    UTTERANCE_REGEX = r"\s+(?P<utterance>.*)\s+"
+    TIMING_REGEX = r"\D(?P<timing>\d{1,9}_\d{1,9})\D"
+    LINE_REGEX = PARTICIPANT_REGEX + UTTERANCE_REGEX + TIMING_REGEX
+
+    SPACER_REGEX = r"\((?P<spacer>[\d.]+)\)"
+
     def _pla_reader(self) -> pylangacq.Reader:
         return pylangacq.read_chat(self._path)
 
@@ -12,25 +20,52 @@ class ChaFile(InputFile):
         return self._pla_reader().headers()[0]
 
     def _extract_utterances(self):
-        reader = self._pla_reader().utterances(by_files=False)
-        return [self._to_utterance(read_utterance) for read_utterance in reader]
+        with open(self._path, encoding="utf-8") as f:
+            lines = f.readlines()
+        utterance_info = [self._extract_info(
+            line) for line in lines if not line.startswith("@")]
 
-    @classmethod
-    def _to_utterance(cls, read_utterance) -> Utterance:
-        """Convert pylangacq Utterance to sktalk Utterance"""
-        participant = read_utterance.participant
-        time = read_utterance.time_marks
-        time = list(time) if isinstance(time, (list, tuple)) else None
-        text = str(read_utterance.tiers)
-        text = cls._clean_utterance(text)
-        return Utterance(
-            participant=participant,
-            time=time,
-            utterance=text,
-        )
+        # collect all utterance info in a terrible, terrible loop
+        collection = []
+        participant = None
+        for info in utterance_info:
+            if info["utterance"] is None:
+                continue
+            if info["participant"] is not None:
+                participant = info["participant"]
+            complete_utterance = Utterance(
+                participant=participant,
+                time=info["time"],
+                utterance=info["utterance"])
+            collection.append(complete_utterance)
+        return collection
 
-    @classmethod
-    def _clean_utterance(cls, utterance):
+    @staticmethod
+    def _extract_info(line):
+        default_return = {"utterance": None}
+
+        extract_re = re.search(ChaFile.LINE_REGEX, line)
+        if not extract_re:
+            return default_return
+
+        try:
+            utterance = ChaFile._clean_utterance(extract_re["utterance"])
+        except TypeError:
+            return default_return
+
+        if utterance is not None:
+            timing = ChaFile._clean_timing(extract_re["timing"])
+            return ({
+                "participant": extract_re["participant"],
+                "time": timing,
+                "utterance": utterance
+            })
+        return default_return
+
+    @staticmethod
+    def _clean_utterance(utterance):
+        if re.match(ChaFile.SPACER_REGEX, utterance):
+            return None
         utterance = str(utterance)
         utterance = re.sub(r"^([^:]+):", "", utterance)
         utterance = re.sub(r"^\s+", "", utterance)
@@ -44,3 +79,8 @@ class ChaFile(InputFile):
         utterance = re.sub(r" {2}", " ", utterance)
         utterance = re.sub(r"[ \t]{1,5}$", "", utterance)
         return utterance
+
+    @staticmethod
+    def _clean_timing(timing):
+        timing = timing.split("_")
+        return [int(t) for t in timing] if len(timing) == 2 else None
--- tests/corpus/parsing/test_cha.py
+++ tests/corpus/parsing/test_cha.py
@@ -10,9 +10,26 @@ def path_source():
 
 @pytest.fixture
 def cha_info():
-    n_utterances = 13
-    participants = {'A', 'B'}
-    return n_utterances, participants
+    n_utterances = 15
+    utterance_first = "first line of utterance"
+    utterance_last = "spaced (.) with multiple (2.4) spacers"
+    participants = {'MS. A', 'BertramKIBBEL'}
+    timing = [[0, 1500],
+              [1500, 2775],
+              [2775, 3773],
+              [4052, 5515],
+              [4052, 5817],
+              [6140, 9487],
+              [9487, 12888],
+              [12888, 14050],
+              [14050, 17014],
+              [17014, 17800],
+              [17700, 18611],
+              [18611, 21090],
+              [19011, 20132],
+              [21090, 23087],
+              [24457, 25746]]
+    return n_utterances, utterance_first, utterance_last, participants, timing
 
 
 @pytest.fixture
@@ -50,19 +67,27 @@ def expected_metadata():
 
 class TestChaFile:
     def test_parse(self, path_source, cha_info, expected_metadata):
-        n_utterances, participants = cha_info
+        expected_n_utterances, expected_first, expected_last, expected_participants, expected_timing = cha_info
         cha_utts, cha_meta = ChaFile(path_source).parse()
         assert cha_meta == expected_metadata
-        assert {u.participant for u in cha_utts} == participants
-        assert len(cha_utts) == n_utterances
+        assert cha_utts[0].utterance == expected_first
+        assert cha_utts[-1].utterance == expected_last
+        assert {u.participant for u in cha_utts} == expected_participants
+        assert len(cha_utts) == expected_n_utterances
+        parsed_timing = [utt.time for utt in cha_utts]
+        assert parsed_timing == expected_timing
 
     def test_wrapped_parser(self, path_source, cha_info, expected_metadata):
-        n_utterances, participants = cha_info
+        expected_n_utterances, expected_first, expected_last, expected_participants, expected_timing = cha_info
         parsed_cha = Conversation.from_cha(path_source)
         assert isinstance(parsed_cha, Conversation)
         assert parsed_cha.metadata == expected_metadata
-        assert {u.participant for u in parsed_cha.utterances} == participants
-        assert len(parsed_cha.utterances) == n_utterances
+        assert parsed_cha.utterances[0].utterance == expected_first
+        assert parsed_cha.utterances[-1].utterance == expected_last
+        assert {u.participant for u in parsed_cha.utterances} == expected_participants
+        assert len(parsed_cha.utterances) == expected_n_utterances
+        parsed_timing = [utt.time for utt in parsed_cha.utterances]
+        assert parsed_timing == expected_timing
 
     unclean_clean = [
         [
