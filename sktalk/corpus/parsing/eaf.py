d7b7750770cc2d0478e230b9e742cb20ed1d4ffb
Barbara Vreede <b.vreede@esciencecenter.nl>
2024-01-03 20:41:26 +0100

    set up basis of eaf parser

--- sktalk/corpus/parsing/eaf.py
+++ sktalk/corpus/parsing/eaf.py
@@ -1,7 +1,11 @@
-from speach import elan
+from ..utterance import Utterance
+from .parser import InputFile
 
-class ChaFile(InputFile):
-    eaf = elan.read_eaf(filename)
-    for tier in eaf:
-        for ann in tier:
-            _df = _df.append(pd.DataFrame({'begin': ann.from_ts, 'end' : ann.to_ts, 'participant' : tier.ID, 'utterance' : ann.text, 'source': path+filename}, index=[0]), ignore_index=True)
+
+class EafFile(InputFile):
+
+    def _extract_metadata(self):
+        return {}
+
+    def _extract_utterances(self):
+        return []
