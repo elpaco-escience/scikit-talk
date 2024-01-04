83a49b6a6dbca2a99a6e724635659988ed975ae1
Barbara Vreede <b.vreede@esciencecenter.nl>
2024-01-04 09:14:56 +0100

    reduce hardcoding by assigning variables at the top of the class

--- sktalk/corpus/parsing/eaf.py
+++ sktalk/corpus/parsing/eaf.py
@@ -1,9 +1,22 @@
+import re
 import xml.etree.ElementTree as eTree
 from ..utterance import Utterance
 from .parser import InputFile
 
 
 class EafFile(InputFile):
+    """Parser for .eaf files."""
+    # known values for the attributes
+    TIER = "TIER"
+    TIER_ID = "TIER_ID"
+    ALIGNABLE_ANNOTATION = "ALIGNABLE_ANNOTATION"
+    ANNOTATION_VALUE = "ANNOTATION_VALUE"
+    TIME_SLOT_REF1 = "TIME_SLOT_REF1"
+    TIME_SLOT_REF2 = "TIME_SLOT_REF2"
+    TIME_ORDER = "TIME_ORDER"
+    TIME_SLOT = "TIME_SLOT"
+    TIME_SLOT_ID = "TIME_SLOT_ID"
+    TIME_VALUE = "TIME_VALUE"
 
     def _extract_metadata(self):
         return {}
@@ -14,14 +27,14 @@ class EafFile(InputFile):
         # once again, a terrible loop >.<
         annotations, sorting = [], []
 
-        for tier in self.root.findall(".//TIER"):
-            participant = tier.get("TIER_ID")
+        for tier in self.root.findall(f".//{self.TIER}"):
+            participant = tier.get(self.TIER_ID)
 
-            for annotation in tier.findall(".//ALIGNABLE_ANNOTATION"):
-                utterance = annotation.find("ANNOTATION_VALUE").text
+            for annotation in tier.findall(f".//{self.ALIGNABLE_ANNOTATION}"):
+                utterance = annotation.find(self.ANNOTATION_VALUE).text
 
-                begin = annotation.get("TIME_SLOT_REF1")
-                end = annotation.get("TIME_SLOT_REF2")
+                begin = annotation.get(self.TIME_SLOT_REF1)
+                end = annotation.get(self.TIME_SLOT_REF2)
                 time = [timedict[begin], timedict[end]]
 
                 annotations.append(
@@ -30,7 +43,9 @@ class EafFile(InputFile):
                         utterance=utterance,
                         time=time
                     ))
-                sorting.append(int(begin.replace("ts", "")))
+
+                order = re.search(r"\d+", begin)
+                sorting.append(int(order.group()))
 
         return [utt for _, utt in sorted(zip(sorting, annotations))]
 
@@ -43,5 +58,6 @@ class EafFile(InputFile):
 
     @staticmethod
     def _extract_times(root):
-        time_slots = root.find(".//TIME_ORDER").findall(".//TIME_SLOT")
-        return {stamp.get("TIME_SLOT_ID"): int(stamp.get("TIME_VALUE")) for stamp in time_slots}
+        time_slots = root.find(
+            f".//{EafFile.TIME_ORDER}").findall(f".//{EafFile.TIME_SLOT}")
+        return {stamp.get(EafFile.TIME_SLOT_ID): int(stamp.get(EafFile.TIME_VALUE)) for stamp in time_slots}
