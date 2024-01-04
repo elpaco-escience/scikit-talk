d869cab04e4c4dc16b7ed809558cfde9f61a86c2
Barbara Vreede <b.vreede@esciencecenter.nl>
2024-01-04 08:57:53 +0100

    parser works in loop

--- sktalk/corpus/parsing/eaf.py
+++ sktalk/corpus/parsing/eaf.py
@@ -1,6 +1,6 @@
+import xml.etree.ElementTree as ET
 from ..utterance import Utterance
 from .parser import InputFile
-import xml.etree.ElementTree as ET
 
 
 class EafFile(InputFile):
@@ -9,24 +9,30 @@ class EafFile(InputFile):
         return {}
 
     def _extract_utterances(self):
+        timedict = self._extract_times(self.root)
+
         # once again, a terrible loop >.<
-        annotations = []
+        annotations, sorting = [], []
 
         for tier in self.root.findall(".//TIER"):
             participant = tier.get("TIER_ID")
 
             for annotation in tier.findall(".//ALIGNABLE_ANNOTATION"):
+                utterance = annotation.find("ANNOTATION_VALUE").text
+
                 begin = annotation.get("TIME_SLOT_REF1")
                 end = annotation.get("TIME_SLOT_REF2")
-                utterance = annotation.find("ANNOTATION_VALUE").text
+                time = [timedict[begin], timedict[end]]
 
                 annotations.append(
                     Utterance(
                         participant=participant,
                         utterance=utterance,
-                        time=[begin, end]
+                        time=time
                     ))
-        return annotations
+                sorting.append(int(begin.replace("ts", "")))
+
+        return [utt for _, utt in sorted(zip(sorting, annotations))]
 
     @property
     def root(self):
@@ -34,3 +40,8 @@ class EafFile(InputFile):
             tree = ET.parse(self._path)
             self._root = tree.getroot()
         return self._root
+
+    @staticmethod
+    def _extract_times(root):
+        time_slots = root.find(".//TIME_ORDER").findall(".//TIME_SLOT")
+        return {stamp.get("TIME_SLOT_ID"): int(stamp.get("TIME_VALUE")) for stamp in time_slots}
